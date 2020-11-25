e117d4715f0615ea58053c75562638b833c53da1
hyesungoh <haesungoh414@gmail.com>
2020-11-25 22:19:04 +0900

    nav design, check scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
-function Navigation() {
-    return (
-        <div className="navigation">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </div>
-    );
+class Navigation extends React.Component {
+    componentDidMount() {
+        this.changeNavOpacity();
+        
+    }
+
+    changeNavOpacity = () => {
+        const nav = document.querySelector(".navigation");
+        window.onscroll = () => {
+            if (window.pageYOffset > 100) {
+                nav.style.opacity = "0.5";
+            } else {
+                nav.style.opacity = "1";
+            }
+        };
+    }
+
+    render() {
+        return (
+            <div className="navigation">
+                <div className="logo">
+                    <h1 className="navi__logo">Ohcha</h1>
+                </div>
+
+                <div className="center"></div>
+                <div className="link">
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Navigation;
