2d633865a4e3aae9ccde656ef15a9380578737b2
hyesungoh <haesungoh414@gmail.com>
2020-11-27 20:56:58 +0900

    design about

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./About.css";
 
 function About() {
     return (
-        <div>
-            <h3>This is About</h3>
+        <div className="about">
+            <div className="about__card">
+                <h3>Oh Hyesung</h3>
+                <div>
+                    <h5>github.com/hyesungoh</h5>
+                    <h5>haesungoh414@gmail.com</h5>
+                </div>
+            </div>
         </div>
     );
 }
